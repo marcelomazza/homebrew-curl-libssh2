7c71571c341b2193ab0add8e3f15a37751435e62
Marcelo Mazza <ux@marcelomazza.com.ar>
2019-03-20 18:35:00 -0300

    Build --with-libssh2

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -22,6 +22,7 @@ class Curl < Formula
   keg_only :provided_by_macos
 
   depends_on "pkg-config" => :build
+  depends_on "libssh2" => :build
 
   def install
     system "./buildconf" if build.head?
@@ -35,6 +36,8 @@ class Curl < Formula
       --without-ca-bundle
       --without-ca-path
     ]
+
+    args << "--with-libssh2"
 
     system "./configure", *args
     system "make", "install"
